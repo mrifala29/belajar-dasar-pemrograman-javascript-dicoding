e325d995aebbc1c1830d18a4a2ce0c1516021d9a
mrifala29 <mrifala29@gmail.com>
2025-10-29 05:54:36 +0700

    Update task 4

--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,5 +1,15 @@
 function fibonacci(n) {
-
+  if (n === 0) {
+    return [0];
+  }
+  if (n === 1) {
+    return [0, 1];
+  }
+  
+  const prev = fibonacci(n - 1);
+  prev.push(prev[prev.length - 1] + prev[prev.length - 2]);
+  
+  return prev;
 }
 
 // Jangan hapus kode di bawah ini!
